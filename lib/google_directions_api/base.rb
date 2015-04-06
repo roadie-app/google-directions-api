083f65b2053e6acb01eba333add09a6dc2b5587e
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2015-04-06 12:11:51 -0400

    Setup request logging

--- lib/google_directions_api/base.rb
+++ lib/google_directions_api/base.rb
@@ -7,7 +7,11 @@ module GoogleDirectionsAPI
 
     def get(path, params)
       params.merge!({ key: key })
-      conn.get path, params
+      conn.get(path, params).tap do |response|
+        logger.send(log_level, "Url: #{response.env["url"].to_s}")
+        logger.send(log_level, "Status: #{response.env["status"].to_s}")
+        logger.send(log_level, "Body: #{response.body}")
+      end
     end
 
     private
@@ -18,6 +22,14 @@ module GoogleDirectionsAPI
 
     def key
       ENV['GOOGLE_API_KEY']
+    end
+
+    def logger
+      GoogleDirectionsAPI.configuration.logger
+    end
+
+    def log_level
+      GoogleDirectionsAPI.configuration.log_level
     end
   end
 end
--- lib/google_directions_api/configuration.rb
+++ lib/google_directions_api/configuration.rb
@@ -1,5 +1,16 @@
+require 'logger'
+
 module GoogleDirectionsAPI
   class Configuration
-    attr_accessor :logger
+    attr_writer :logger
+    attr_writer :log_level
+
+    def logger
+      @logger ||= Logger.new('/dev/null')
+    end
+
+    def log_level
+      @log_level ||= :info
+    end
   end
 end
