c4610f63b2b3274aa02732ec725baa1cb9193741
Chris Ibarra <cibarra@Chriss-MacBook-Pro-2.local>
2018-08-06 15:46:34 -0400

    [#159375928] Bug fix for distance and duration if waypoints are present

--- lib/google_directions_api/directions.rb
+++ lib/google_directions_api/directions.rb
@@ -15,22 +15,20 @@ module GoogleDirectionsAPI
       end
     end
 
-    def distance
-      meters = data["routes"][0]["legs"][0]["distance"]["value"]
-      meters * 0.000621371
-    end
-
-    def duration
-      seconds = data["routes"][0]["legs"][0]["duration"]["value"]
-      seconds / 60
-    end
-
     def polyline
       data["routes"][0]["overview_polyline"]["points"]
     end
 
+    def distance
+      total_distance * 0.000621371
+    end
+
+    def duration
+      total_duration / 60
+    end
+
     def has_tolls?
-      data["routes"][0]["legs"][0]["steps"].any? { |x| x["html_instructions"].try(:downcase).try(:include?, 'toll road') }
+      tolls_along_route?
     end
 
     private
@@ -44,16 +42,56 @@ module GoogleDirectionsAPI
           origin: from,
           destination: to,
           waypoints: encode_waypoints
-      }.keep_if { |k,v| v.present? }
+      }.keep_if { |k,v| !v.empty? }
     end
 
     def encode_waypoints
-      return unless waypoints.present?
+      return if waypoints.empty?
       "enc:#{GoogleDirectionsAPI::Polyline::Encoder.encode(waypoints)}:"
     end
 
     def data
       @data ||= JSON.parse(response.body)
+    end
+
+    def total_distance
+      if waypoints.empty?
+        data["routes"][0]["legs"][0]["distance"]["value"]
+      else
+        meters = 0
+        data["routes"][0]["legs"].each do |leg|
+          meters += leg["distance"]["value"]
+        end
+        return meters
+      end
+    end
+
+    def total_duration
+      if waypoints.empty?
+        data["routes"][0]["legs"][0]["duration"]["value"]
+      else
+        seconds = 0
+        data["routes"][0]["legs"].each do |leg|
+          seconds += leg["duration"]["value"]
+        end
+        return seconds
+      end
+    end
+
+    def tolls_along_route?
+      if waypoints.empty?
+        data["routes"][0]["legs"][0]["steps"].any? { |x| x["html_instructions"].try(:downcase).try(:include?, 'toll road') }
+      else
+        data["routes"][0]["legs"].each do |leg|
+          next unless leg["steps"].any?
+
+          if leg["steps"]["html_instructions"].try(:downcase).try(:include?, 'toll road')
+            return true
+          end
+        end
+
+        return false
+      end
     end
   end
 end
